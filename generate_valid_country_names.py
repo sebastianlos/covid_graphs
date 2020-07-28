9b616bd927453021e99e29a84425f8563b901f7f
sebastianloschert <sebastian.loschert@gmx.de>
2020-07-28 15:03:26 +0200

    updating generate valid country names.py

--- generate_valid_country_names.py
+++ generate_valid_country_names.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import numpy as np
 
 conf = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+
+country_names_array = conf["Country/Region"].unique()
+
+with open("country_names.txt", "w") as f:
+    for row in country_names_array:
+        f.write(row + "\n")
