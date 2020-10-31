3f8b3564c608e4504cebe36e65e7ff3c45b89adb
sebastianloschert <sebastian.loschert@gmx.de>
2020-10-31 23:39:00 +0100

    completing third subplot

--- covid_graphs2.py
+++ covid_graphs2.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+
 def clean_data(df):
     """
     This function cleans and transposes the dataframe.
@@ -23,7 +24,8 @@ def clean_data(df):
     df.index.name = "Date"
     return df
 
-# read files
+
+# get data from john hopkins university
 conf = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/\
 COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/\
 time_series_covid19_confirmed_global.csv")
@@ -61,92 +63,31 @@ INFO_PROMPT = "Data for {} for last {} days\
 \n(Last data ingestion: {})"\
     .format(COUNTRY, DAYS_TO_LOOK_BACK, chosen_days[-1])
 
-conf.set_index(["Country/Region", "Province/State"], inplace=True)
-conf.drop(["Lat", "Long"], axis=1, inplace=True)
-conf = conf.transpose()
-conf.index = pd.to_datetime(conf.index)
-conf.index.name = "Date"
-
-conf["United Kingdom"].apply(sum, axis=1).plot()
-
-death
-
-
-# per COUNTRY
-country_conf = conf.groupby("Country/Region").get_group(COUNTRY)
-country_death = death.groupby("Country/Region").get_group(COUNTRY)
-country_conf
-
-# on chosen dates
-conf_numbers = []
-death_numbers = []
-for date in chosen_days:
-    conf_numbers.append(country_conf[date].sum())
-    death_numbers.append(country_death[date].sum())
-
-# deathrate in last x days
-incrDList = []
-for i in range(1, DAYS_TO_LOOK_BACK):
-    if death_numbers[-i-1] != 0:
-        INCR = (death_numbers[-i] - death_numbers[-i-1])/death_numbers[-i-1]
-    else:
-        # instead of infinite: 0
-        INCR = 0
-    incrDList.append(INCR*100)
-incrDList = incrDList[::-1]
-
-# rate of new confirmed cases
-incrCList = []
-for i in range(1, DAYS_TO_LOOK_BACK):
-    if conf_numbers[-i-1] != 0:
-        INCR = (conf_numbers[-i] - conf_numbers[-i-1])/conf_numbers[-i-1]
-    else:
-        INCR = 0                # instead of infinite: 0
-    incrCList.append(INCR*100)
-incrCList = incrCList[::-1]
+conf = clean_data(conf)
+death = clean_data(death)
 
 # PLOT
 plt.figure(figsize=(8, 8))
 plt.suptitle(INFO_PROMPT)
 
-ax1 = plt.subplot(2, 2, 1)
-conf_plot = conf.set_index(["Country/Region", "Province/State"]).loc[COUNTRY].\
-    iloc[:, -DAYS_TO_LOOK_BACK:].sum()
-line1, = ax1.plot_date(pd.to_datetime(conf_plot.keys().values),
-                       conf_plot.values, "b",
-                       label=f"confirmed: {format(conf_numbers[-1])}")
-ax1.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d"))
-ax1.set_title(COUNTRY + " (confirmed)")
-ax1.tick_params(axis="x", labelrotation=30)
-ax1.legend()
+plt.subplot(2, 2, 1)
+conf[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:].plot(color="b")
+plt.title(COUNTRY + " (confirmed)")
 
+plt.subplot(2, 2, 2)
+death[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:].plot(color="r")
+plt.title(COUNTRY + " (deaths)")
 
-ax2 = plt.subplot(2, 2, 2)
-death_plot = death.set_index(["Country/Region",
-                              "Province/State"]).loc[COUNTRY].\
-    iloc[:, -DAYS_TO_LOOK_BACK:].sum()
-ax2.plot_date(pd.to_datetime(death_plot.keys().values),
-              death_plot.values, "r",
-              label=f"death: {format(death_numbers[-1])}")
-ax2.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d"))
-ax2.plot([], [], " ", label="(deaths/conf = {:.2%})"
-         .format(death_numbers[-1]/conf_numbers[-1]))
-ax2.set_title(COUNTRY + " (deaths)")
-ax2.tick_params(axis="x", labelrotation=30)
-ax2.legend()
-
-
-ax3 = plt.subplot(2, 1, 2)
-ax3.plot(np.arange(DAYS_TO_LOOK_BACK - 1), incrDList,
-         label="death growth rate")
-ax3.plot(np.arange(DAYS_TO_LOOK_BACK - 1), incrCList, label="new cases rate")
-ax3.set_title(COUNTRY + " (rates)")
-ax3.set_xlabel("Days")
-ax3.set_ylabel("Percentage")
-# calculating ylim from mean
-# plt.ylim(0, max(pd.Series(incrCList).mean(),
-#                 pd.Series(incrDList).mean()) * 3.5)
-ax3.legend()
+plt.subplot(2, 1, 2)
+(conf[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:].diff()*100 /
+    conf[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:]).\
+        plot(label="cases growth rate")
+(death[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:].diff()*100 /
+    death[COUNTRY].apply(sum, axis=1)[-DAYS_TO_LOOK_BACK:]).\
+        plot(label="death growth rate")
+plt.title(COUNTRY + " (rates)")
+plt.ylabel("Percentage")
+plt.legend()
 
 plt.tight_layout()
 plt.show()
